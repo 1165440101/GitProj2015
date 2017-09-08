2c5b8993c397ab1dbdc61cf72e4e98b8b08bd4b3
zhaopp <zhaopp@yiche.com>
2017-09-08 17:13:04 +0800

    create proj

---
+++ GitProj2015/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitProj2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2;
+            Console.Write(num);
+        }
+    }
+}
